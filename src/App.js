14ccfdb0f34a5882a81b636aa16642e8fd383bbe
Jacqueline <proyectos@branta.com.co>
2021-06-17 20:25:15 -0500

    first contador

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+constructor(){
+  super()
+  this.state={contador : 0}
+  this.onKeyUpHandler=this.onKeyUpHandler.bind(this)
+}
+  onKeyUpHandler(event){  
+    this.setState({ 
+     contador: event.target.value.length     
+    })
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea id="inc" rows="3" onKeyUp={ this.onKeyUpHandler }></textarea>
+        <div className="counter">{this.state.contador}</div>
       </div>
     );
   }
